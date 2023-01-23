5174de207551d7c8d4b0f6a9cb18437b1ef7e071
Snami <Pressionem@gmail.com>
2023-01-23 16:04:11 +0500

    Starting project

---
+++ ExcelTable/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ExcelTable;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ExcelTable/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ExcelTable;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
